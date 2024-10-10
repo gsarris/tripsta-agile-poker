7fcbaca33c20a39a0f456a723e9e190f3273f38a
Georgios Sarris <gsarris@gmail.com>
2024-10-10 10:16:17 +0300

    Updates

--- app/src/main/java/com/tripsta/tripstapoker/card/GridDecoration.java
+++ app/src/main/java/com/tripsta/tripstapoker/card/GridDecoration.java
@@ -1,8 +1,9 @@
 package com.tripsta.tripstapoker.card;
 
 import android.graphics.Rect;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
 
 public class GridDecoration extends RecyclerView.ItemDecoration {
 
